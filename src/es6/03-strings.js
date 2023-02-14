c59677efe03efefea1dd37266a1ec73c815ef09e
danselo <danselo185@gmail.com>
2023-02-14 09:47:44 -0500

    Add string explain ES6

---
+++ src/es6/03-strings.js
@@ -0,0 +1,17 @@
+let hello = "Hello";
+let world = "World"
+//this is the normal concatenete 
+
+let pharase1 = hello+ " " + world+ "!";
+
+// ES6 TEMPLATE LITERAL 
+let pharase2 = `${hello} ${world}!`
+
+console.log(pharase1);
+
+//MULTI-LINE 
+let completePharase1 = "Hello \n" + "Bye";
+let completePharase2 = `This is the last text with the ES6 
+    Continue...
+`
+console.log(completePharase2);
