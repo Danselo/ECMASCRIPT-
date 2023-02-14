a0c7f7ad26227770b4264f5186559a5ccf17612c
danselo <danselo185@gmail.com>
2023-02-14 16:53:27 -0500

    Objet values and Object entries ES8

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,4 @@
+let countries = {CO: "Colombia", MX: "Mexico", CL : "Chile"};
+console.log(Object.entries(countries));
+
+//convert all in array
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,5 @@
+let countries = {CO: "Colombia", MX: "Mexico", CL : "Chile"};
+console.log(Object.values(countries));
+
+
+//convert the values in array
