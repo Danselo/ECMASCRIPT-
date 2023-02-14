d6039893cf3822bb0132d3025c23ffde0591dbe0
danselo <danselo185@gmail.com>
2023-02-14 17:16:54 -0500

    trailing commas and String paddings examples in ES8

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,6 @@
+let string = "Hello";
+console.log(string.padStart(6,"_")); //_Hello
+console.log(string.padStart(8,"_")); //___Hello
+
+console.log(string.padEnd(6,"_")); //Hello_
+console.log(string.padEnd(8,"_"));//Hello___
---
+++ src/es8/03-trailing-commands.js
@@ -0,0 +1,8 @@
+// const array= [1,2,3,4 ];
+// console.log(array);
+// console.log(array.length);
+
+//add trailing commas
+const array= [1,2,3,4, , , , ,10 ]; 
+console.log(array); // [1,2,3,4, <4 empty items>, 10]
+console.log(array.length); // 9
